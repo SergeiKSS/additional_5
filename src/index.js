b32536699d599c7e4eecf1cecaf90c6816763b76
Sergei Karpovich <SergeiKSS@mail.ru>
2017-09-24 22:38:26 +0300

    brackets:implement check with stack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,60 @@
 module.exports = function check(str, bracketsConfig) {
   // My solution Sergei Karpovich
-  let count1 = 0, count2 = 0;
+
+let stack = [];
+  let i = 0, j =0, k=0;
+  let open = false, close = false;
+
+  while(i < str.length){
+  	j = 0; 
+  	k = 0;
+  	//поиск открывающейся скобки
+	while (j < bracketsConfig.length) {
+		if (str[i]==bracketsConfig[j][0]) {
+			open = true;
+			break;
+		}
+		j++;
+	}
+	//поиск закрывающей скобки
+	while (k < bracketsConfig.length) {
+		if (str[i]==bracketsConfig[k][1]) {
+			close = true;
+			break;
+		}
+		k++;
+	}
+	if (open & close) {
+		if (stack[stack.length-1] == str[i]) {
+			stack.pop();
+		} else {
+			stack.push(str[i]);	
+		}
+		i++;
+		open = false;
+		close = false;
+		continue;
+	}
+	//добавляем в стек
+	if (open) {
+		stack.push(str[i]);
+	}
+	if (close && stack[stack.length-1]==bracketsConfig[k][0]) {
+		stack.pop();
+	}
+  	i++;
+  	open = false;
+  	close = false;
+  }
+
+
+  if (stack.length == 0) {
+  	return true;
+  }	else {
+	return false;  	
+  }
+
+ /* let count1 = 0, count2 = 0;
   for (let i = 0; i <= bracketsConfig.length-1; i++) {
   	for (let j = 0; j <= str.length-1; j++) {
   		if (str[j] === bracketsConfig[i][0]) {
@@ -16,5 +70,5 @@ module.exports = function check(str, bracketsConfig) {
   }
   else{
   	return true;
-  }
+  }*/
 }
