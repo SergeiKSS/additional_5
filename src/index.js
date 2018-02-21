b790090ca59f785fd0029fdae6b721386beef9f5
Sergei Karpovich <SergeiKSS@mail.ru>
2018-02-21 22:14:42 +0300

    brackets: implement function check

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 module.exports = function check(str, bracketsConfig) {
   // My solution Sergei Karpovich
-
 let stack = [];
   let i = 0, j =0, k=0;
   let open = false, close = false;
@@ -54,21 +53,4 @@ let stack = [];
 	return false;  	
   }
 
- /* let count1 = 0, count2 = 0;
-  for (let i = 0; i <= bracketsConfig.length-1; i++) {
-  	for (let j = 0; j <= str.length-1; j++) {
-  		if (str[j] === bracketsConfig[i][0]) {
-  			count1 +=1;
-  		}
-  		if (str[j] === bracketsConfig[i][1]) {
-  			count2 +=1;
-  		}
-  	}
-  }
-  if (count1!==count2) {
-  	return false;
-  }
-  else{
-  	return true;
-  }*/
 }
